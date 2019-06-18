beb7dd338f0a1a3e514d17fa76833b7389b03860
Tetiana <Tatyana.granchenko@gmail.com>
2019-06-18 21:41:55 +0300

    added image from Kingfisher framework

--- Les_13_CocoaPods_Animation/ViewController.swift
+++ Les_13_CocoaPods_Animation/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher // cashing images in memory cache and disk cache
 
 class ViewController: UIViewController {
 
@@ -14,9 +15,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // image literal from assets (Shift + Command + M, than double click and chose image)
+        // let image1 = UIImage(named: "placeholder")
+        // let image3: UIImage = #imageLiteral(resourceName: "placeholder")
+        let image2: UIImage =  #imageLiteral(resourceName: "placeholder.jpg")
+     
+        let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/d/dd/Big_%26_Small_Pumkins.JPG")
+          imageView.kf.setImage(with: url, placeholder: image2)
     }
-
-
 }
 
